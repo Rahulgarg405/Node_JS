5353b1d1797987f756a9565beb1a9024e68f801b
Rahul garg <rgarg3577@gmail.com>
2025-06-28 20:48:59 +0530

    MongoDB Basics

---
+++ MongoDB_Basics/app.js
@@ -0,0 +1,58 @@
+// password : q0lMhpBfwHcpJBHr
+
+// mongodb+srv://rgarg3577:q0lMhpBfwHcpJBHr@cluster0.kdkrgd8.mongodb.net/
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://rgarg3577:q0lMhpBfwHcpJBHr@cluster0.kdkrgd8.mongodb.net/"
+  )
+  .then(() => console.log("Database connected Successfully"))
+  .catch((e) => console.log(e));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+  isActive: Boolean,
+  tags: [String],
+  createdAt: { type: Date, default: Date.now },
+});
+
+const User = mongoose.model("User", userSchema);
+
+async function runQueryExample() {
+  try {
+    // const newUser = await User.create({
+    //   name: "john doe",
+    //   email: "john@gmail.com",
+    //   age: "69",
+    //   isActive: false,
+    //   tags: ["designer", "developer"],
+    // });
+    // console.log(newUser);
+
+    // const users = await User.find({ isActive: false });
+
+    // It will give all users name and email only :
+    // const selectedFields = await User.find().select("name email -_id");
+
+    const limitedUsers = await User.find().limit(3).skip(1);
+
+    // it will give users in sorted order by age in descending order,
+    // if we give {age : 1} => order will be ascending;
+    // const sortedUsers = await User.find().sort({ age: -1 });
+
+    // console.log(sortedUsers);
+
+    const count = await User.countDocuments({ isActive: true });
+    console.log(count);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await mongoose.connection.close();
+  }
+}
+
+runQueryExample();
